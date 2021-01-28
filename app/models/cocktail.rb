837fa602f1bf42d8a12061e92178a0635ead9d9a
raissa borba <raissavborba@gmail.com>
2021-01-28 18:36:51 +0100

    saving

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-    has_many :dose
-    has_many :ingredient, through: :dose
-    validates :name, uniqueness: true
+    has_many :doses, dependent: :destroy
+    has_many :ingredients, through: :doses
+    validates :name, presence: true, uniqueness: true
 end
